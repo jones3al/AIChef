19a774c5a3114795b2e9e89203e97703b7e8aede
jones3al <Alex_L_Jones@carmax.com>
2023-12-07 15:24:44 -0500

    create additional pages and build api call

---
+++ Server/Controllers/RecipeController.cs
@@ -0,0 +1,42 @@
+﻿using AiChef.Server.Data;
+using AIChef.Client.Shared;
+using AIChef.Server.Services;
+using AIChef.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIChef.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+
+        private readonly IOpenAIAPI _openAIService;
+
+        public RecipeController(IOpenAIAPI openAIService)
+        {
+            _openAIService = openAIService;
+        }
+
+
+        [HttpPost, Route("GetRecipeIdeas")]
+        public async  Task<ActionResult<List<Idea>>> GetRecipeIdeas(RecipeParms recipeParms)
+        {
+            string mealtime = recipeParms.MealTime;
+            List<string> ingredients = recipeParms.Ingredients
+                                                  .Where(x => !string.IsNullOrEmpty(x.Description))
+                                                  .Select(x=> x.Description!)
+                                                  .ToList();
+            if (string.IsNullOrEmpty(mealtime) )
+            {
+                mealtime = "Breakfast";
+            }
+            
+            var ideas = await _openAIService.CreateRecipeIdeas(mealtime, ingredients);
+
+            return ideas;
+            //return SampleData.RecipeIdeas;
+        }
+    }
+}
---
+++ Server/Services/IOpenAIAPI.cs
@@ -0,0 +1,9 @@
+﻿using AIChef.Shared;
+
+namespace AIChef.Server.Services
+{
+    public interface IOpenAIAPI
+    {
+        Task<List<Idea>> CreateRecipeIdeas(string mealtime, List<string> ingredients);
+    }
+}
